5c92c577da7351b4dd614ab9df45964192b1d7e1
itzpandit0001-create <itz.pandit0001@gmail.com>
2026-01-27 12:40:53 +0530

    Add calculator functions for display and operations

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 function openLink(link){
   window.open(link,"_blank");
 }
+
+
+// Calculator display
+const calcDisplay = document.getElementById("calculator-display");
+
+// Append number/operator
+function calcAppend(val){
+  if(calcDisplay.innerText==="0" || calcDisplay.innerText==="Error")
+    calcDisplay.innerText="";
+  calcDisplay.innerText += val;
+}
+
+// Clear display
+function calcClear(){
+  calcDisplay.innerText = "0";
+}
+
+// Backspace last char
+function calcBack(){
+  calcDisplay.innerText = calcDisplay.innerText.slice(0,-1);
+  if(calcDisplay.innerText==="") calcDisplay.innerText="0";
+}
+
+// Calculate expression
+function calcCalculate(){
+  try{
+    const exp = calcDisplay.innerText.replace('%','/100');
+    calcDisplay.innerText = eval(exp);
+  }catch{
+    calcDisplay.innerText = "Error";
+  }
+}
